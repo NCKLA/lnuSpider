0dfc0e2d40d92c601f9f517a3bc367328c8afe75
just123cz <1035965337@qq.com>
2020-04-09 11:24:09 +0800

    4月9日 同花顺高管介绍栏---部分代码

---
+++ jqka_com_executive_introduction/jqka_com_executive_introduction/pipelines.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import time
+import json
+from jqka_com_executive_introduction import settings
+import urllib3
+from scrapy.exporters import JsonLinesItemExporter
+
+
+class JqkaComExecutiveIntroductionPipeline(object):
+    def __init__(self):
+        now_time = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
+        self.fp = open(r"C:\python\lnuSpider\data\json\同花顺上市公司高管介绍" + now_time + ".json", 'wb')
+        self.exporter = JsonLinesItemExporter(self.fp, ensure_ascii=False)
+        # self.http = urllib3.PoolManager()
+
+    def open_spider(self, spider):
+        print("=====爬虫开始力=====")
+
+    def process_item(self, company_detail, spider):
+        # dir_path = '%s/%s' % (settings_wzh.IMAGES_STORE, "sohucaijing")
+        # # 下载图片
+        # for src in item['images_src'][0]:
+        #     file_path = '%s/%s' % (dir_path, src.split('/')[-1])
+        #     r = self.http.request('GET', src)
+        #     with open(file_path, 'wb') as f:
+        #          f.write(r.data)
+        #     f.close()
+        self.exporter.export_item(company_detail)
+        return company_detail
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.fp.close()
+        print("=====爬虫结束力=====")
