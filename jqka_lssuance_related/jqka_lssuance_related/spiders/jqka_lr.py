d8e1b8b28d35607b6a9ea5154de40e5270da27cc
just123cz <1035965337@qq.com>
2020-04-21 10:30:58 +0800

    4月21日 发行相关完工

--- jqka_lssuance_related/jqka_lssuance_related/spiders/jqka_lr.py
+++ jqka_lssuance_related/jqka_lssuance_related/spiders/jqka_lr.py
@@ -33,25 +33,64 @@ class JqkaLrSpider(scrapy.Spider):
         sheet = book.active
         data = []
         row_num = 1
-        while row_num <= 500:
+        while row_num <= 10:
             # 将表中第一列的1-100行数据写入data数组中
             data.append(sheet.cell(row=row_num, column=3).value)
             row_num = row_num + 1
         for i in data:
-            company_detail = JqkaLssuanceRelatedItem()
+            company_lr = JqkaLssuanceRelatedItem()
             url = 'http://basic.10jqka.com.cn/%s' % i + '/company.html'
-            company_detail['url'] = url
+            company_lr['url'] = url
             # print(response.text)
-            yield scrapy.Request(company_detail['url'],
-                                 meta={'company_detail': company_detail}, callback=self.detail_ni, dont_filter=True)
+            yield scrapy.Request(company_lr['url'],
+                                 meta={'company_lr': company_lr}, callback=self.detail_ni, dont_filter=True)
         return
 
     def detail_ni(self, response):
         print("=====准备详细情况中的第一部分信息====")
-        company_detail = response.meta['company_detail']
+        company_lr = response.meta['company_lr']
         root = response.xpath("//div[@class='content page_event_content']")[0]
         # print(root)
-        company_detail['name'] = root.xpath("//div[@stat][@id='detail']//table[@class='m_table']//tr[1]""//td[2]//spa"
+        company_lr['name'] = root.xpath("//div[@stat][@id='detail']//table[@class='m_table']//tr[1]""//td[2]//spa"
                                             "n/text()")[0].extract()
+        content1 = response.xpath("//div[@id='publish'][@stat='company_publish']/div[@class='bd pr']/"
+                                  "table[@class='m_table']//tr[1]")
+
+        content11 = content1.xpath("./td[1]/span/text()").getall()
+        print(content11)
+        content12 = content1.xpath("./td[2]/span/text()").getall()
+        print(content12)
+        content13 = content1.xpath("./td[3]/span/text()").getall()
+        print(content13)
+
+        content2 = response.xpath("//div[@id='publish'][@stat='company_publish']/div[@class='bd pr']/"
+                                  "table[@class='m_table']//tr[2]")
+        content21 = content2.xpath("./td[1]/span/text()").getall()
+        print(content21)
+        content22 = content2.xpath("./td[2]/span/text()").getall()
+        print(content22)
+        content23 = content2.xpath("./td[3]/span/text()").getall()
+        print(content23)
+
+        content3 = response.xpath("//div[@id='publish'][@stat='company_publish']/div[@class='bd pr']/"
+                                  "table[@class='m_table']//tr[3]")
+        content31 = content3.xpath("./td[1]/span/text()").getall()
+        print(content31)
+        content32 = content3.xpath("./td[2]/span/text()").getall()
+        print(content32)
+        content33 = content3.xpath("./td[3]/span/text()").getall()
+        print(content33)
+
+        content4 = response.xpath("//div[@id='publish'][@stat='company_publish']/div[@class='bd pr']/"
+                                  "table[@class='m_table']//tr[4]")
+        content41 = content4.xpath("//div[@class='main_sell'][1]/span/text()").getall()
+        print(content41)
+        content42 = content4.xpath("//div[@class='main_sell'][2]/span/text()").getall()
+        print(content42)
+
+        content5 = response.xpath("//div[@id='publish'][@stat='company_publish']//p[@class='tip lh24']/text()").getall()
+        content5 = "".join(content5).strip()
+        print(content5)
+
         time.sleep(random.randint(2, 5))
-        yield company_detail
+        yield company_lr
